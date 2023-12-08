02f6605373804b54211f8d2a748ca3920b8ab9b6
Jessemccallums <jessemccallums2001@gmail.com>
2023-12-08 00:31:42 -0400

    adding changes

--- front/bethesda-quest-front/src/app/home/components/home.jsx
+++ front/bethesda-quest-front/src/app/home/components/home.jsx
@@ -6,8 +6,21 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.home}>
+      <div className={styles.titlebox}>
+        <h1 className={styles.title}>
+          Welcome To <br />Bethesda Quest
+        </h1>
+      </div>
         <div>
-          <Image src="./bg_home.svg" width={200} height={200} className={styles.bg}   alt="" />
+          <Image src="./bg_home.svg" layout='fill' className={styles.bg}   alt="" />
+        </div>
+        <div>
+          <Image src="./card-home.svg" width={200} height={200} className={styles.bg1}   alt="" />
+        </div>
+        <div>
+          <button className={styles.btn_img}>
+            <Image src="./btn.svg" width={100} height={100} className={styles.btn}/>
+          </button>
         </div>
     </div>
   )
